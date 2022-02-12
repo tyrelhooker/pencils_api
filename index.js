38674829a19df4c48230f18b4277595dd78992a8
T.J. Hooker <tyrelhooker@gmail.com>
2022-02-12 15:19:03 -0500

    Populate initial database data

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-import express from 'express';
-require('dotenv').config();
-import db from './db.json';
+const express = require('express');
+const db = require('./db.json');
+const app = express();
 
-console.log(db);
+app.use(express.json());
+
+const pencils = db;
+
+app.get('/api/pencils', (req, res) => {
+  res.json(pencils);
+});
+
+app.get('/api/pencils/:company', (req, res) => {
+  console.log(req.params.company);
+  const company = req.params.company;
+  console.log(company);
+  const companyName = pencils.find(companyName => companyName.company === company);
+  console.log(companyName);
+  res.json(companyName);
+})
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
